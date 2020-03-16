e9c7cf1f44e790abb8d09deb1d0afe7050e4353f
Siabell <valesiabatto@gmail.com>
2020-03-15 23:40:17 -0500

    permitir cors

--- src/main/java/edu/escuelaing/arep/WebApp.java
+++ src/main/java/edu/escuelaing/arep/WebApp.java
@@ -12,6 +12,28 @@ public class WebApp {
 	public static void main(String[] args) {
 		port(getPort());
 		staticFiles.location("/web");
+		options("/*",
+	        (request, response) -> {
+	
+	            String accessControlRequestHeaders = request
+	                    .headers("Access-Control-Request-Headers");
+	            if (accessControlRequestHeaders != null) {
+	                response.header("Access-Control-Allow-Headers",
+	                        accessControlRequestHeaders);
+	            }
+	
+	            String accessControlRequestMethod = request
+	                    .headers("Access-Control-Request-Method");
+	            if (accessControlRequestMethod != null) {
+	                response.header("Access-Control-Allow-Methods",
+	                        accessControlRequestMethod);
+	            }
+	
+	            return "OK";
+	        });
+
+		before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
 		final Gson gson = new Gson();
 		UserService  userService = new UserServiceImpl();
 		System.out.println("entra antes post y get");
